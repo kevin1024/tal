39de4239ac75a6ad833baf3b1a5e748dc36e95c7
Kevin McCarthy <me@kevinmccarthy.org>
2013-09-25 21:18:16 -1000

    enforce min leinengen version

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :uberjar-name "tal-standalone.jar"
+  :min-lein-version "2.0.0"
   :dependencies [
       [org.clojure/clojure "1.5.1"]
       [enlive "1.1.4"]
